4892d5c90efe383fa42973467ffd6411c99babf5
钟舒艺 <1820965203@qq.com>
2022-12-08 20:18:51 +0800

    ✨ feat(custom): 添加windicss

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -1,12 +1,13 @@
 import { PluginOption } from 'vite'
 
 import vue from '@vitejs/plugin-vue'
+import WindiCSS from 'vite-plugin-windicss'
 
 import { configHtmlPlugin } from './html'
 
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   /** 插件数组 */
-  const vitePlugins: (PluginOption | PluginOption[])[] = [vue()]
+  const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), WindiCSS()]
 
   // vite-plugin-html
   vitePlugins.push(configHtmlPlugin(viteEnv, isBuild))
